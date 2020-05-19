290a775f6c44deac573ea4c7c06336ed5b46b9fa
Arunabh <arunabh.matrix@gmail.com>
2020-05-18 23:46:56 -0500

    Still working on bakend

--- Routes/users.js
+++ Routes/users.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const { check, validationResult } = require('express-validator');
 const router = express.Router();
-
+const bcrypt = require('bcryptjs');
 const User = require('../models/User');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 
 // @route       POST api/users
 // @desc        Register a user
@@ -14,12 +16,43 @@ router.post(
 		check('email', 'Please include a valid email').isEmail(),
 		check('password', 'Please enter a password with min 6 characters').isLength({ min: 6 })
 	],
-	(req, res) => {
+	async (req, res) => {
 		const errors = validationResult(req);
 		if (!errors.isEmpty()) {
 			return res.status(400).json({ errors: errors.array() });
 		}
-		res.send('passed');
+		const { name, email, password } = req.body;
+
+		try {
+			let user = await User.findOne({ email });
+
+			if (user) {
+				return res.status(400).json({ msg: 'User already exists !' });
+			}
+
+			user = new User({ name, email, password });
+
+			const salt = await bcrypt.genSalt(10);
+
+			//console.log(password);
+
+			user.password = await bcrypt.hash(password, salt);
+
+			await user.save();
+
+			const payload = {
+				user : { id: user.id }
+			};
+
+			jwt.sign(payload, config.get('jwtsecret'), { expiresIn: 36000 }, (err, token) => {
+				if (err) throw err;
+				res.json({token});
+			});
+
+		} catch (err) {
+			console.error(err.message);
+			res.status(500).send('server error');
+		}
 	}
 );
 
